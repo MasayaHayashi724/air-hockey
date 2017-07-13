90850c13aa20ba5bcfe364ba725402b088595040
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-07-13 15:17:01 +0900

    Apply impulse to the ball

--- air-hockey/GameScene.swift
+++ air-hockey/GameScene.swift
@@ -11,7 +11,21 @@ import GameplayKit
 
 class GameScene: SKScene {
 
+    var main = SKSpriteNode()
+    var enemy = SKSpriteNode()
+    var ball = SKSpriteNode()
+
     override func didMove(to view: SKView) {
+        self.main = childNode(withName: "main") as! SKSpriteNode
+        self.enemy = childNode(withName: "enemy") as! SKSpriteNode
+        self.ball = childNode(withName: "ball") as! SKSpriteNode
+
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        border.friction = 0
+        border.restitution = 1
+        self.physicsBody = border
+
+        self.ball.physicsBody?.applyImpulse(CGVector(dx: -30, dy: -30))
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
